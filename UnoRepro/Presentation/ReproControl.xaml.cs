89a5854af4380e9275487ae81f416cd4ec0d4055
NSFW <nsfw@outlook.com>
2026-02-13 23:13:42 -0800

    Put a UserControl in each tab - no repro.

---
+++ UnoRepro/Presentation/ReproControl.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Input;
+
+namespace UnoRepro.Presentation;
+
+public sealed partial class ReproControl : UserControl
+{
+    public ReproControl()
+    {
+        this.InitializeComponent();
+    }
+}
